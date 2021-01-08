7cd59cd4ee0200c107cf415c070a3377bba612b2
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-01-07 16:43:40 -0800

    password length and case selection prompts work, but need to return user if invalid value is entered.

--- Develop/script.js
+++ Develop/script.js
@@ -7,17 +7,45 @@ let numbers = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 
 // prompt user to enter number of password characters
-
+let passwordLength = prompt("Enter desired length of your new password between 8 and 128 characters.");
 // value must be between 8 and 128
+if (passwordLength > 7 && passwordLength < 129){
+    value = passwordLength;
+    console.log(passwordLength);
+}
+else if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter valid password length, between 8 and 128 characters.")
+} 
+// else if (passwordLength === "") {
+// // if left blank then generate random number between 8 - 128
+// }
 
-// if left blank then generate random number between 8 - 128
 
-
+// CASE PROMPT BEGIN
 
 // prompt user for lower case, upper case, or both
-
+let caseSel = prompt("Would you like you password to contain lower case letters, upper case letters or both?  Please enter '1' for lower case, '2' for upper case, or '3' for both.");
 // record user's response if valid
 
+  // use switch case to carry out action
+  caseSel = parseInt(caseSel);
+  switch (caseSel) {
+    case 1:
+      caseSel = lowerCase;
+      break;
+    case 2:
+      caseSel = upperCase;
+      break;
+    case 3:
+      caseSel = lowerCase.concat(upperCase);
+      break;
+    default:
+      alert('You did not pick a valid option. Try again.');
+      // return;
+      break;
+    }
+
+console.log(caseSel);
 // else if user enters invalid response, alert that an incorrect response was entered
 
 // restart function if response is invalid
